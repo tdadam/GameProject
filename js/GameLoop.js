3a428b5191881414e0f7c61f60dd471e4f61bf9d
Tim Adam <tdadam@xactware.com>
2015-11-11 09:26:09 -0700

    Added css file, started loop file

--- js/GameLoop.js
+++ js/GameLoop.js
@@ -1,3 +1,28 @@
 /**
  * Created by tdadam on 11/11/15.
  */
+var canvas, renderingContext, width, height, sprite;
+
+function main() {
+    canvasSetup();
+    document.body.appendChild(canvas);
+    loadGraphics();
+}
+
+function canvasSetup(){
+    canvas = document.createElement("canvas");
+    canvas.style.border = "1px solid black";
+    canvas.width = 300;
+    canvas.height = 500;
+    renderingContext = canvas.getContext("2d");
+}
+
+function loadGraphics(){
+    sprite = new Image();
+    sprite.src = "./images/bird.png";
+    sprite.onload = function() {
+        renderingContext.fillStyle = "#70C5CF";
+        renderingContext.fillRect(0, 0, 300, 500);
+        renderingContext.drawImage(sprite, 10, 10);
+    }
+}
