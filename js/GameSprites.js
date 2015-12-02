3c6ef18cd25ee83be9263705277d2425a6c0b69d
Tim Adam <tdadam@xactware.com>
2015-12-02 09:23:28 -0700

    cleaned up sprite js

--- js/GameSprites.js
+++ js/GameSprites.js
@@ -2,18 +2,14 @@
  * Created by tdadam on 11/11/15.
  */
 // Sprite variables
-
 var
     mechSprite,
     backgroundSprite,
     foregroundSprite,
     topObstacleSprite,
-    bottomObstacleSprite,
-    splashScreenSprite,
-    okButtonSprite;
+    bottomObstacleSprite;
 
 // Sprite constructor
-
 function Sprite(img, x, y, width, height) {
     this.img = img;
     this.x = x * 2;
@@ -23,13 +19,11 @@ function Sprite(img, x, y, width, height) {
 }
 
 // Draw method (prototype)
-
 Sprite.prototype.draw = function (renderingContext, x, y) {
     renderingContext.drawImage(this.img, this.x, this.y, this.width, this.height, x, y, this.width, this.height);
 };
 
 // initialize Sprites
-
 function initSprites(img) {
     mechSprite = [
         new Sprite(img, 8, 5, 33, 38),
@@ -37,11 +31,11 @@ function initSprites(img) {
         new Sprite(img, 8, 88, 33, 38)
     ];
 
+    //background image and moving ground
     backgroundSprite = new Sprite(img, 0, 162, 250, 223);
     foregroundSprite = new Sprite(img, 0, 389, 250, 32);
 
-    //okButtonSprite = new Sprite(img, 189, 4, 43, 44);
-    okButtonSprite = new Sprite(img, 192, 52, 37, 37);
+    //obstacles
     topObstacleSprite = new Sprite(img, 114, 1, 50, 152);
     bottomObstacleSprite = new Sprite(img, 50, 1, 50, 152);
 }
